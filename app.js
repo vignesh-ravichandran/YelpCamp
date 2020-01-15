1d4ffef651722fd9f48ee1886ace4b99ea763d92
vignesh ravichandran <vigneshravichandran27@gmail.com>
2020-01-15 12:56:57 +0000

    p;alds

--- app.js
+++ app.js
@@ -63,6 +63,8 @@ app.use("/campgrounds/:id/comments",commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use(authRoutes);
 
+app.listen(process.env.PORT,process.env.IP,function(){console.log("app.started")});
+/*
 app.listen(3000, function(){
    console.log("The YelpCamp Server Has Started!");
-});
+}); */ 
